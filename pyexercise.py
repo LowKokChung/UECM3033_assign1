21c93d419f7b4c7c1f50a890f618db9bf7bc5961
LowKokChung <kokchung2010@1utar.my>
2016-01-27 22:57:38 +0800

    Updated pyexercise.py

--- pyexercise.py
+++ pyexercise.py
@@ -1,24 +1,23 @@
 import sympy as sy
 import numpy as np
 
-def fun_1( your_id ):
-    ans = hex(your_id)
+def fun_1( 1206226  ):
+    ans = hex(1206226)
     return ans
 
 def my_integral():
     x = sy.Symbol('x')
-    ans = sy.integrate( sy.exp(-x**2), (x,0, sy.oo))
+    ans = sy.integrate( sy.1/(2*sqrt(pi))exp(-x**2), (x,0, sy.oo))
     return ans
 
 def my_solution():
-    A = np.array( [[3,1], [1,2]] )
-    b = np.array([9,8])
+    A = np.array( [[3,1,5,9,8], [1,2,4,7,6]] )
+    b = np.array([21,48])
     x = np.linalg.solve(A,b) # Solve Ax = b
     return x
 
 
 if __name__ == '__main__':
-    your_id = 12345678
-    print('Hexadecimal representation of %d is %s'%( your_id, fun_1( your_id) ))
+    print('Hexadecimal representation of %d is %s'%( 1206226, fun_1(1206226) ))
     print('Integral = ', my_integral())
     print('Solution = ', my_solution())
